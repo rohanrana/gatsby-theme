396b65f96679492f8f32e4ad2dba6131568df6d8
dvshah26 <46349256+dvshah26@users.noreply.github.com>
2020-06-09 15:04:45 +0530

    chnages in contact and index of default theme.

--- themes/default/src/pages/contact.js
+++ themes/default/src/pages/contact.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import "core/src/assets/css/style.scss";
 
 export default class Contact extends Component {
   render() {
     return (
       <div>
         <h1>This stuff is from contact us from default theme</h1>
+        <h1 className="coreThemeCheck">
+          This style is applied from core theme in Default theme,.
+        </h1>
       </div>
     );
   }
